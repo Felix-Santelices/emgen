77ddf1a352b6207d4d14a5a764281058c7a1a7db
Felix-Santelices <felix.santelices@edu.sait.ca>
2024-04-09 12:25:25 -0600

    first commit

---
+++ emgen.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 22 19:22:15 2023
+
+@author: SHREYA
+"""
+
+import openai
+import streamlit as st
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai.api_key  = os.getenv("key") 
+
+def generateEmail(prompt):
+    response = openai.completions.create(
+        model = "gpt-3.5-turbo-instruct",
+        prompt = prompt,
+        temperature = 0.7,
+        max_tokens = 256,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0)
+    return response.choices[0].text
+
+
+st.title("Email Generator")
+fromem = st.text_input("Email From", value = 'SAIT')
+toem  = st.text_input("Email To", value = 'SAIT Student')
+about = st.text_input("What's The Email About?", value= 'Request a reply with their login credentials')
+
+prompt = "write an email from "  + fromem  + " to " + toem + " about " + about
+
+if st.button('Make My Email'):
+    emgen = generateEmail(prompt)
+    #st.write(emgen)
+    text = st.text_area("Make Changes:", value = emgen, height=500)
+    #generateEmail("Emailt to Ray to Say Hello"))
